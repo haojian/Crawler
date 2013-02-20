542a3d3d9f72bfa0b05290f2834b199e04b5cb7c
Haojian Jin <haojian.jin@gmail.com>
2013-02-20 14:35:11 -0800

    update

---
+++ src/yelp/YelpImageExtractor.java
@@ -0,0 +1,13 @@
+package yelp;
+
+public class YelpImageExtractor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/yelp/YelpReviewExtractor.java
@@ -0,0 +1,13 @@
+package yelp;
+
+public class YelpReviewExtractor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
