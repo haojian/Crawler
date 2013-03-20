eae79ebaac8a60d870ebd53ecf522e6746e6c19a
Haojian Jin <haojian.jin@gmail.com>
2013-03-20 16:03:16 -0700

    update

--- src/yelp/YelpReviewExtractor.java
+++ src/yelp/YelpReviewExtractor.java
@@ -86,7 +86,7 @@ public class YelpReviewExtractor {
 		String filename = "";
 		String content = "";
 		try {
-			doc = Jsoup.connect(target_url).timeout(0).get();
+			doc = Jsoup.connect(target_url).userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:5.0) Gecko/20100101 Firefox/5.0").timeout(0).get();
 			String title = doc.title();
 			System.out.println(title);
 			// extract restaurant name, 
